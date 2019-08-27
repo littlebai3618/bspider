40b3d16a04eec46da21cff6471e6f0bae9471a6e
baishanglin <baishanglin@renrenche.com>
2019-08-27 15:28:51 +0800

    v0.1 beta 版本 2019-08-27 修改install.py beta 0.1 bug: format参数错误导致脚本运行异常

--- bin/gunicorn.py
+++ bin/gunicorn.py
@@ -4,7 +4,7 @@
 # @Use     :
 
 
-def make_gunicorn_conf(path):
+def make_gunicorn_conf(path, **kwargs):
     gunicorn = """import gevent.monkey
     gevent.monkey.patch_all()
     loglevel = {loglevel}
@@ -15,4 +15,4 @@ def make_gunicorn_conf(path):
     worker_class = 'gevent'
     x_forwarded_for_header = 'X-FORWARDED-FOR'"""
     with open(f'{path}/cache/supervisor.conf', 'w') as f:
-        f.write(gunicorn)
+        f.write(gunicorn.format(**kwargs))
--- install.py
+++ install.py
@@ -40,9 +40,9 @@ if __name__ == '__main__':
         supervisor_password=frame_settings.SUPERVISOR_RPC[1],
         supervisor_port=frame_settings.SUPERVISOR_RPC[2],
     )
-    process_type = sys.argv[1]
-    make_supervisor_conf(root_path, process_type)
-    make_gunicorn_conf(root_path)
+    process_type = 'single_node'
+    make_supervisor_conf(root_path, process_type, **init_dict)
+    make_gunicorn_conf(root_path, **init_dict)
 
     with open(f'{root_path}/start.sh', 'w') as f:
         f.write(bash % (root_path, root_path, root_path))
