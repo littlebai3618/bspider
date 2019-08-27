0150a0cfd15cde5f1f33fd9f1ed618ca2c7dbae3
baishanglin <baishanglin@renrenche.com>
2019-08-27 15:58:54 +0800

    v0.1 beta 版本 2019-08-27 修改install.py beta 0.1.2 bug: 修复自动生成配置文件格式错乱的bug

--- bin/gunicorn.py
+++ bin/gunicorn.py
@@ -6,13 +6,13 @@
 
 def make_gunicorn_conf(path, **kwargs):
     gunicorn = """import gevent.monkey
-    gevent.monkey.patch_all()
-    loglevel = {loglevel}
-    bind = '{agent_service}'
-    pidfile = '{root_path}/cache/gunicorn.pid'
-    workers = 1
-    worker_connections = 2000
-    worker_class = 'gevent'
-    x_forwarded_for_header = 'X-FORWARDED-FOR'"""
+gevent.monkey.patch_all()
+loglevel = {loglevel}
+bind = '{agent_service}'
+pidfile = '{root_path}/cache/gunicorn.pid'
+workers = 1
+worker_connections = 2000
+worker_class = 'gevent'
+x_forwarded_for_header = 'X-FORWARDED-FOR'"""
     with open(f'{path}/cache/supervisor.conf', 'w') as f:
         f.write(gunicorn.format(**kwargs))
--- bin/supervisor.py
+++ bin/supervisor.py
@@ -6,66 +6,66 @@
 
 def make_supervisor_conf(path, install_type='single_node', **kwargs):
     base = """[unix_http_server]                                                                                                                                                                             
-    file={root_path}/cache/supervisor.sock;
-    [supervisord]
-    logfile={root_path}/log/supervisord.log;
-    loglevel={loglevel};
-    pidfile={root_path}/cache/supervisord.pid;
-    [rpcinterface:supervisor]
-    supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
-    [supervisorctl]
-    serverurl=unix://{root_path}/cache/supervisor.sock ;
+file={root_path}/cache/supervisor.sock;
+[supervisord]
+logfile={root_path}/log/supervisord.log;
+loglevel={loglevel};
+pidfile={root_path}/cache/supervisord.pid;
+[rpcinterface:supervisor]
+supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
+[supervisorctl]
+serverurl=unix://{root_path}/cache/supervisor.sock ;
 
-    [inet_http_server]
-    port=0.0.0.0:{supervisor_port}
-    username={supervisor_username}
-    password={supervisor_password}
+[inet_http_server]
+port=0.0.0.0:{supervisor_port}
+username={supervisor_username}
+password={supervisor_password}
 
     """
 
     agent = """[program:agent]
-    command=gunicorn -c {root_path}/cache/gunicorn.py --chdir {root_path}/ agent_manager:app;
-    directory={root_path}
-    user=bspider
-    startsecs=0;
-    stopwaitsecs=0;
-    autostart=false;
-    autorestart=true;
-    stdout_logfile={root_path}/log/agent.log;
-    stderr_logfile={root_path}/log/agent.log;"""
+command=gunicorn -c {root_path}/cache/gunicorn.py --chdir {root_path}/ agent_manager:app;
+directory={root_path}
+user=bspider
+startsecs=0;
+stopwaitsecs=0;
+autostart=false;
+autorestart=true;
+stdout_logfile={root_path}/log/agent.log;
+stderr_logfile={root_path}/log/agent.log;"""
 
     master = """[program:master]
-    command=gunicorn -c {root_path}/cache/gunicorn.py --chdir {root_path}/ master_manager:app;
-    directory={root_path}
-    user=bspider
-    startsecs=0;
-    stopwaitsecs=0;
-    autostart=true;
-    autorestart=true;
-    stdout_logfile={root_path}/log/master.log;
-    stderr_logfile={root_path}/log/master.log;"""
+command=gunicorn -c {root_path}/cache/gunicorn.py --chdir {root_path}/ master_manager:app;
+directory={root_path}
+user=bspider
+startsecs=0;
+stopwaitsecs=0;
+autostart=true;
+autorestart=true;
+stdout_logfile={root_path}/log/master.log;
+stderr_logfile={root_path}/log/master.log;"""
 
     bcorn = """[program:bcorn]
-    command=python {root_path}/bcorn_manager.py;
-    directory={root_path}
-    user=bspider
-    startsecs=0;
-    stopwaitsecs=0;
-    autostart=true;
-    autorestart=true;
-    stdout_logfile={root_path}/log/bcorn.log;
-    stderr_logfile={root_path}/log/bcorn.log;"""
+command=python {root_path}/bcorn_manager.py;
+directory={root_path}
+user=bspider
+startsecs=0;
+stopwaitsecs=0;
+autostart=true;
+autorestart=true;
+stdout_logfile={root_path}/log/bcorn.log;
+stderr_logfile={root_path}/log/bcorn.log;"""
 
     scheduler = """[program:scheduler]
-    command=python {root_path}/scheduler_manager.py;
-    directory={root_path}
-    user=bspider
-    startsecs=0;
-    stopwaitsecs=0;
-    autostart=true;
-    autorestart=true;
-    stdout_logfile={root_path}/log/bcorn.log;
-    stderr_logfile={root_path}/log/bcorn.log;"""
+command=python {root_path}/scheduler_manager.py;
+directory={root_path}
+user=bspider
+startsecs=0;
+stopwaitsecs=0;
+autostart=true;
+autorestart=true;
+stdout_logfile={root_path}/log/bcorn.log;
+stderr_logfile={root_path}/log/bcorn.log;"""
     if install_type == 'single_node':
         with open(f'{path}/cache/supervisor_single_node.conf', 'w') as f:
             f.write(f'{base}\n{agent}\n{master}\n{scheduler}\n{bcorn}'.format(**kwargs))
