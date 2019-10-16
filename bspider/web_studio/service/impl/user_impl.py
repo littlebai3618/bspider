37b9414d4ffa20e9c7cf1200036eb968364e750b
baishanglin <baishanglin@renrenche.com>
2019-10-16 19:05:59 +0800

    v19.10.16.1 beta 版本 2019-10-16 1. 增加用户名长度 2. 增加用户状态返回

--- bspider/web_studio/service/impl/user_impl.py
+++ bspider/web_studio/service/impl/user_impl.py
@@ -20,8 +20,8 @@ class UserImpl(BaseImpl):
 
     def get_user_by_id(self, id):
         """暂时先这样，后面增加复杂密码验证"""
-        sql = f'select `id`, `identity`, `username`, `role`, `email`, `phone`, `create_time`, `update_time`  ' \
-            f'from {self.table_name} where `id`=%s and `status`=1;'
+        sql = f'select `id`, `identity`, `username`, `role`, `email`, `phone`, `status`, `create_time`, `update_time`  ' \
+            f'from {self.table_name} where `id`=%s;'
         return self.handler.select(sql, id)
 
     def add_user(self, data):
