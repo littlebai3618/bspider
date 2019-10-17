9422ecb260fc3e86b2bad0f5a7a58196ee62f214
baishanglin <baishanglin@renrenche.com>
2019-10-17 16:21:43 +0800

    v19.10.17.2 beta 版本 2019-10-17 1. 增加用户状态支持 2. 增加Code 分页功能

--- bspider/web_studio/controller/code.py
+++ bspider/web_studio/controller/code.py
@@ -5,25 +5,27 @@
 from flask import Blueprint
 
 from bspider.core.api import auth
-from .validators.user_forms import RegisterForm
 from bspider.web_studio.service.code import CodeService
 from .validators.code_forms import AddForm, UpdateForm
-
+from .validators import PageForm
 
 code = Blueprint('code_bp', __name__)
 
 code_service = CodeService()
+
 
 @code.route('/code/<int:code_id>', methods=['GET'])
 @auth.login_required
 def get(code_id):
     return code_service.get_code(code_id)
 
+
 @code.route('/code', methods=['GET'])
 @auth.login_required
 def gets():
-    form = RegisterForm()
+    form = PageForm()
     return code_service.get_codes(**form.get_dict())
+
 
 @code.route('/code', methods=['POST'])
 @auth.login_required
@@ -31,10 +33,12 @@ def add():
     form = AddForm()
     return code_service.add(form.name.data, form.description.data, form.type.data, form.content.data, form.editor.data)
 
+
 @code.route('/code/<int:code_id>', methods=['DELETE'])
 @auth.login_required
 def delete(code_id):
     return code_service.delete(code_id)
+
 
 @code.route('/code', methods=['PATCH'])
 @auth.login_required
