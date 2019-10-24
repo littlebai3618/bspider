9ccb95538d9e070ea70269469ddf66a9ee56cf04
baishanglin <baishanglin@renrenche.com>
2019-10-24 16:31:31 +0800

    v19.10.24.7 beta 版本 1. 修复node 接口kwargs 为空时的错误 2. 修复node update接口启动、停止两种操作错乱的问题, 修复worker update接口status=1失效的bug 3. agent启动worker进程为daemon 类型 4. node list接口增加node_id 返回 5. 修复新增worker接口异常bug 6. 增加节点关闭时的校验功能 7. 修复更新worker接口异常bug, 修复下载器解析器日志名称异常问题

--- bspider/web_studio/service/impl/node_impl.py
+++ bspider/web_studio/service/impl/node_impl.py
@@ -102,7 +102,7 @@ class NodeImpl(BaseImpl):
         return self.handler.update(sql, values)
 
     def get_worker(self, worker_id):
-        sql = f'select `id`, `ip`, `name`, `type`, `description`, `coroutine_num`, `create_time`, `update_time` from {self.worker_table} where `id`={worker_id};'
+        sql = f'select `id`, `ip`, `name`, `type`, `description`, `coroutine_num`, `status`, `create_time`, `update_time` from {self.worker_table} where `id`={worker_id};'
         log.debug(f'SQL:{sql}')
         return self.handler.select(sql)
 
@@ -110,10 +110,10 @@ class NodeImpl(BaseImpl):
         start = (page - 1) * limit
         fields = self.make_search(search)
         if len(fields):
-            sql = f'select `id`, `ip`, `name`, `type`, `description`, `coroutine_num`, `create_time`, `update_time` ' \
+            sql = f'select `id`, `ip`, `name`, `type`, `description`, `coroutine_num`, `status`, `create_time`, `update_time` ' \
                   f'from {self.worker_table} where {fields} order by `id` {sort} limit {start},{limit};'
         else:
-            sql = f'select `id`, `ip`, `name`, `type`, `description`, `create_time`, `coroutine_num`, `update_time` ' \
+            sql = f'select `id`, `ip`, `name`, `type`, `description`, `coroutine_num`, `status`, `create_time`, `update_time` ' \
                   f'from {self.worker_table} order by `id` {sort} limit {start},{limit};'
         log.debug(f'SQL:{sql}')
         return self.handler.select(sql), self.total_num(search, self.worker_table)
