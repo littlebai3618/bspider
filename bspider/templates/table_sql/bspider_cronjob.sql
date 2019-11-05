5d92f79593603d399ec492d569a75272193b4c30
baishanglin <baishanglin@renrenche.com>
2019-11-05 17:12:05 +0800

    v19.11.5.2 beta 版本 1. 修复Cron模块接收错误参数的bug 2. 修改cron table 脚本

--- bspider/templates/table_sql/bspider_cronjob.sql
+++ bspider/templates/table_sql/bspider_cronjob.sql
@@ -32,7 +32,7 @@ CREATE TABLE `bspider_cron` (
   `trigger_type` varchar(10) NOT NULL COMMENT '定时任务执行间隔类别 cron ...',
   `next_run_time` double DEFAULT NULL COMMENT '任务的下次执行时间',
   `func` text NOT NULL COMMENT '实际执行时调用的方法名称',
-  `executor` varchar(10) NOT NULL COMMENT '选择执行者，默认是通过线程池执行',
+  `executor` varchar(20) NOT NULL COMMENT '选择执行者，默认是通过线程池执行',
   `status` int(2) NOT NULL COMMENT '内部任务状态 0无操作任务，1需新增的任务，2需更新的任务，3需删除的任务',
   `description` text NOT NULL COMMENT '当前定时任务的说明文字',
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
