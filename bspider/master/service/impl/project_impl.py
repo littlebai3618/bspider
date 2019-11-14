f53f1a4b28fa74c9e45f4da6564bfe8fc0112287
baishanglin <baishanglin@renrenche.com>
2019-11-14 11:20:08 +0800

    v19.11.13.4 beta 版本 1. 修复任务无法更新的bug

--- bspider/master/service/impl/project_impl.py
+++ bspider/master/service/impl/project_impl.py
@@ -58,7 +58,7 @@ class ProjectImpl(BaseImpl):
     def add_project_binds(self, cids, pid):
         values = ', '.join([f'({pid}, {cid})' for cid in cids])
         sql = f'replace insert into `{self.p2c_table}`(`project_id`, `customcode_id`) ' \
-              f'values {values};'
+              f'values({values});'
         log.debug(sql)
         return sql,
 
