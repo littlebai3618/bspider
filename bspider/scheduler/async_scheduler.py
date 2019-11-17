661fce61027b76019cb23b923171a28eff78ee2b
baishanglin <baishanglin@renrenche.com>
2019-11-17 12:31:53 +0800

    v19.11.17.1 beta 版本 1. 协程抢占导致死锁

--- bspider/scheduler/async_scheduler.py
+++ bspider/scheduler/async_scheduler.py
@@ -37,8 +37,7 @@ class AsyncScheduler(object):
         cur_slice = int(int(now.strftime('%S')) // (60 / 12) + 1)
         self.log.debug(f'cur_slice: {cur_slice}')
 
-        queue_is_full = await self.__is_full_queue(self.__downloader_queue, self.rate)
-        if not (self.rate < 1 or queue_is_full):
+        if not (self.rate < 1 or await self.__is_full_queue(broker, self.__downloader_queue, self.rate)):
             # 本次调度数量
             rate_slice = self.rate / 12
             self.log.debug(f'rate_slice: {rate_slice} real plan schedule num {int(rate_slice * 4)}')
@@ -51,52 +50,9 @@ class AsyncScheduler(object):
                     break
         self.log.debug(f'scheduler->{self.project_id} schedule cycle {cur_loop_sign} finish!')
 
-
-    # async def do_work(self):
-    #     while True:
-    #         all_queue_is_none = False
-    #         now = datetime.datetime.now()
-    #         hash_name = 'scheduler-' + now.strftime('%H%M')
-    #         # 表示新的调度周期
-    #         if hash_name != self.timer.get('hash_name', ''):
-    #             self.timer.clear()
-    #             self.timer['hash_name'] = hash_name
-    #
-    #         cur_slice = int(int(now.strftime('%S')) // (60 / 12) + 1)
-    #
-    #         for project_name, rate in self.monitor.projects.items():
-    #             # 查看速度显示是否已过,由于按照分钟进行划分，所以速度不支持小于每分钟1次的下载速度
-    #             candidate_queue = 'candidate_{}'.format(project_name)
-    #             download_queue = 'download_{}'.format(project_name)
-    #             if rate < 1 or await self.__is_full_queue(download_queue, rate):
-    #                 continue
-    #             else:
-    #                 all_queue_is_none = True
-    #                 # 本次调度数量
-    #                 rate_slice = rate / 12
-    #
-    #                 for i in range(int(rate_slice * 4)):
-    #                     # 获取项目本分钟内已经推送的数量,和当前时间段的需要推送量比较
-    #                     if int(rate_slice * cur_slice) > int(self.timer.get(project_name, 0)):
-    #                         tag, body = await self.mq_handler.pull_new_message(candidate_queue)
-    #                         if tag is not None:
-    #                             if await self.mq_handler.send_message('download', project_name, body):
-    #                                 self.log.info(f'send a new task to request queue: project:{project_name}, body:{body}')
-    #                                 await self.mq_handler.report_acknowledgment(tag)
-    #                                 self.timer[project_name] = self.timer.get(project_name, 0) + 1
-    #                             else:
-    #                                 await self.mq_handler.report_unacknowledgment(tag)
-    #                         else:
-    #                             break
-    #                     else:
-    #                         break
-    #
-    #         if all_queue_is_none:
-    #             await asyncio.sleep(0.5)
-    #
-    async def __is_full_queue(self, queue_name, rate):
+    async def __is_full_queue(self, broker, queue_name, rate):
         """检查任务的下载队列是否阻塞"""
-        msg_count = await self.__broker.mq_handler.get_queue_message_count(queue_name)
+        msg_count = await broker.mq_handler.get_queue_message_count(queue_name)
         threshold = rate // 12
         if threshold < 2:
             threshold = 1
