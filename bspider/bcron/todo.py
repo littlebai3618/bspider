af710c0dc66c910e51b37fb592cc50175aa74eeb
baishanglin <baishanglin@renrenche.com>
2019-11-17 10:32:52 +0800

    v19.11.16.1 beta 版本 1. rabbitmq增加session模式 2. 修改异步发送消息的确认ack流程 *** TODO 定时任务模块增加对协程的支持 done

--- bspider/bcron/todo.py
+++ bspider/bcron/todo.py
@@ -5,48 +5,58 @@
 """
 这个是实际执行的方法，这个方法接收参数，通过参数决定行为
 """
-import asyncio
 import sys
 import traceback
 
 
 from bspider.config import FrameSettings
 from bspider.core import ProjectConfigParser
-from bspider.utils.database.mysql import AioMysqlHandler
+from bspider.utils.database.mysql import MysqlHandler
 from bspider.utils.logger import LoggerPool
 from bspider.utils.importer import import_module_by_code
 from bspider.utils.notify import ding
 
 __frame_settings = FrameSettings()
-__handler = AioMysqlHandler.from_settings(__frame_settings['WEB_STUDIO_DB'])
+__handler = MysqlHandler.from_settings(__frame_settings['WEB_STUDIO_DB'])
 __log = LoggerPool().get_logger(key='bcorn-todo', fn='bcorn', module='bcorn')
 
 
+# def do(**kwargs):
+#     # 这里使用主线程的时间循环，因为异步MYSQL使用默认时间循环
+#     func = None
+#     if kwargs.get('type') == 'operation':
+#         func = run_operation_project
+#     elif kwargs.get('type') == 'crawl':
+#         func = run_spider_project
+#     else:
+#         __log.warning(f'WARNING: UNKNOW CRON JOB PARAMS: {kwargs}')
+#
+#     if func:
+#         loop = asyncio.get_event_loop()
+#         try:
+#             future = asyncio.run_coroutine_threadsafe(func(**kwargs), loop)
+#             loop.run_until_complete(future)
+#             __log.debug('future run success!')
+#         except Exception as e:
+#             tp, msg, tb = sys.exc_info()
+#             e_msg = '> '.join(traceback.format_exception(tp, msg, tb))
+#             e.with_traceback(tb)
+#             __log.error(f'thread event loop error:\n{e_msg}')
+#             ding(f'> {e_msg} \n', 'bcron thread event loop')
+
+
 def do(**kwargs):
-    # 这里使用主线程的时间循环，因为异步MYSQL使用默认时间循环
-    func = None
+    # 模式判断
     if kwargs.get('type') == 'operation':
-        func = run_operation_project
-    elif kwargs.get('type') == 'crawl':
-        func = run_spider_project
+        run_operation_project(**kwargs)
+    elif kwargs.get('type', 'crawl') == 'crawl':
+        run_spider_project(**kwargs)
     else:
         __log.warning(f'WARNING: UNKNOW CRON JOB PARAMS: {kwargs}')
 
-    if func:
-        loop = asyncio.get_event_loop()
-        try:
-            future = asyncio.run_coroutine_threadsafe(func(**kwargs), loop)
-            loop.run_until_complete(future)
-            __log.debug('future run success!')
-        except Exception as e:
-            tp, msg, tb = sys.exc_info()
-            e_msg = '> '.join(traceback.format_exception(tp, msg, tb))
-            e.with_traceback(tb)
-            __log.error(f'thread event loop error:\n{e_msg}')
-            ding(f'> {e_msg} \n', 'bcron thread event loop')
 
 
-async def run_spider_project(project_id, code_id, **kwargs):
+def run_spider_project(project_id, code_id, **kwargs):
     """
     执行自动加载脚本的定时任务
     先检查定时任务状态，如果为1，就执行否则跳出
@@ -57,7 +67,7 @@ async def run_spider_project(project_id, code_id, **kwargs):
     PROJECT_TABLE = __frame_settings['PROJECT_TABLE']
     sql = f'select `id`, `name`, `config`, `status` from {PROJECT_TABLE} where `id`="{project_id}"'
 
-    infos = await __handler.select(sql)
+    infos = __handler.select(sql)
     __log.debug(f'run spider_project cron_job select sql:{sql}')
 
     if not len(infos):
@@ -69,7 +79,7 @@ async def run_spider_project(project_id, code_id, **kwargs):
         __log.warning(f'project_id:{project_id} is not run')
         # ding(f'spider project job:{project_name} is not run')
 
-    run_status, run_msg = await run_corn_job_code(code_id, info['id'], info['name'], info['config'])
+    run_status, run_msg = run_corn_job_code(code_id, info['id'], info['name'], info['config'])
     __log.debug(info['config'])
     if run_status:
         __log.info(run_msg)
@@ -78,8 +88,8 @@ async def run_spider_project(project_id, code_id, **kwargs):
         ding(run_msg, 'crawl task')
 
 
-async def run_operation_project(code_id, **kwargs):
-    run_status, run_msg = await run_corn_job_code(code_id, 0, 'operation', '{"desc": "operation cron job"}')
+def run_operation_project(code_id, **kwargs):
+    run_status, run_msg = run_corn_job_code(code_id, 0, 'operation', '{"desc": "operation cron job"}')
     if run_status:
         __log.info(run_msg)
     else:
@@ -87,10 +97,10 @@ async def run_operation_project(code_id, **kwargs):
         ding(run_msg, 'operation task')
 
 
-async def run_corn_job_code(code_id, project_id, project_name, config):
+def run_corn_job_code(code_id, project_id, project_name, config):
     CODE_STORE_TABLE = __frame_settings['CODE_STORE_TABLE']
     sql = f'select `name`, `content` from {CODE_STORE_TABLE} where `id`="{code_id}"'
-    tmp = await __handler.select(sql)
+    tmp = __handler.select(sql)
     if not len(tmp):
         return False, f'{code_id} is not exist in remote code store'
     content = tmp[0]['content']
@@ -107,7 +117,8 @@ async def run_corn_job_code(code_id, project_id, project_name, config):
             return False, f'{project_id}-{code_id}:\n > {e_msg}'
         try:
             instance = getattr(mod, class_name)(project_config)
-            await instance._exec(func_name='execute_task')
+            # await instance._exec(func_name='execute_task')
+            instance.execute_task()
             return True, f'{project_id}-{code_id} run succeed'
         except Exception:
             tp, msg, tb = sys.exc_info()
