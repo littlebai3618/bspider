c97e67393c8f1f22b67ce18630594cfb6414b8e0
baishanglin <baishanglin@renrenche.com>
2019-11-17 10:43:05 +0800

    v19.11.16.1 beta 版本 1. rabbitmq增加session模式 2. 修改异步发送消息的确认ack流程 *** TODO 定时任务模块增加对协程的支持 done

--- bspider/bcron/base_operation.py
+++ bspider/bcron/base_operation.py
@@ -11,9 +11,8 @@ from bspider.utils.logger import LoggerPool
 class BaseOperation(BaseCustomModule):
 
     def __init__(self, settings: ProjectConfigParser):
-        self.log = LoggerPool().get_logger(key=settings.project_name, fn='bcorn', module='bcorn', project=project_name)
         self.project_name = settings.project_name
-
+        self.log = LoggerPool().get_logger(key=self.project_name, fn='bcorn', module='bcorn', project=self.project_name)
 
     def execute_task(self):
         raise MethodError('you must rebuild execute_task()')
