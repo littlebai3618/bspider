9106e1af15836ce9ea097e7047f0590e04e8f046
baishanglin <baishanglin@renrenche.com>
2019-11-26 16:06:56 +0800

    v19.11.26.2 beta 版本 1. 新增crawl-detail

--- bspider/master/controller/validators/tools_forms.py
+++ bspider/master/controller/validators/tools_forms.py
@@ -20,24 +20,13 @@ class CrawlDetailTagEnum(Enum):
     sign = 'sign'
     url = 'url'
 
-class CrawlDetailSourceEnum(Enum):
-    parser = 'parser'
-    downloader = 'downloader'
-
 
 class GetCrawlDetailForm(BaseForm):
-    source = StringField(validators=[ParamRequired()])
     tag = StringField(validators=[ParamRequired()])
     data = StringField(validators=[ParamRequired()])
 
     def validate_tag(self, value):
         try:
             value.data = CrawlDetailTagEnum(value.data).name
-        except ValueError as e:
-            raise e
-
-    def validate_source(self, value):
-        try:
-            value.data = CrawlDetailSourceEnum(value.data).name
         except ValueError as e:
             raise e
