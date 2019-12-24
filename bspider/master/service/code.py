ad11b54ae152e6f7b7aa3c6504eecd3e03e9d0f4
baishanglin <baishanglin@renrenche.com>
2019-12-24 14:14:21 +0800

    v0.0.2 BETA 版本 修复bug

--- bspider/master/service/code.py
+++ bspider/master/service/code.py
@@ -65,7 +65,7 @@ class CodeService(BaseService, AgentMixIn):
     def delete(self, code_id):
         project_list = self.impl.get_project_by_code_id(code_id)
         if len(project_list):
-            data = [{'id': info['id'], 'name': info['name']} for info in project_list]
+            data = [{'id': info['project_id'], 'name': info['project_name']} for info in project_list]
             log.error(f'delete code:{code_id} failed: can\'t delete in use code')
             return Conflict(msg='can\'t delete in use code', data=data, errno=40004)
         else:
