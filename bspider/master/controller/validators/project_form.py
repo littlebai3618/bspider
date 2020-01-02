25ef79ffa05bfc6adb416d64ced6ad9064913468
baishanglin <baishanglin@renrenche.com>
2020-01-02 19:09:06 +0800

    v0.0.3 BETA 版本 UI 更新

--- bspider/master/controller/validators/project_form.py
+++ bspider/master/controller/validators/project_form.py
@@ -49,7 +49,7 @@ class AddForm(BaseForm):
 
     def validate_config(self, value):
         try:
-            schema(yaml.safe_load(value.data))
+            value.data = (schema(yaml.safe_load(value.data)), value.data)
         except MultipleInvalid as e:
             raise ProjectSettingsError(e.error_message)
 
@@ -60,7 +60,7 @@ class UpdateForm(BaseForm):
 
     def validate_config(self, value):
         try:
-            schema(yaml.safe_load(value.data))
+            value.data = (schema(yaml.safe_load(value.data)), value.data)
         except MultipleInvalid as e:
             raise ProjectSettingsError(e.error_message)
 
--- bspider/master/service/project.py
+++ bspider/master/service/project.py
@@ -13,8 +13,8 @@ class ProjectService(BaseService, AgentMixIn):
     def __init__(self):
         self.impl = ProjectImpl()
 
-    def add(self, editor: str, config: dict, status: int):
-        project = Project(yaml.safe_load(config),
+    def add(self, editor: str, config: list, status: int):
+        project = Project(yaml.safe_load(config[0]),
                           middleware_serializer_method=self.get_middleware_id_by_name,
                           pipeline_serializer_method=self.get_pipeline_id_by_name)
         log.debug(
@@ -31,7 +31,7 @@ class ProjectService(BaseService, AgentMixIn):
                     'description': project.description,
                     'editor': editor,
                     'rate': project.rate,
-                    'config': config,
+                    'config': config[1],
                     'r_config': r_config
                 }
 
@@ -73,7 +73,7 @@ class ProjectService(BaseService, AgentMixIn):
             if not len(infos):
                 return NotFound(msg='project not exist', errno=30001)
 
-            new_project = Project(yaml.safe_load(changes['config']))
+            new_project = Project(yaml.safe_load(changes['config'][0]))
             try:
                 old_project = Project(yaml.safe_load(infos[0]['config']))
             except Exception as e:
@@ -101,6 +101,8 @@ class ProjectService(BaseService, AgentMixIn):
             if False in sign:
                 remote_param['config'] = new_project.dumps()
                 changes['r_config'] = remote_param['config']
+
+        changes['config'] = changes['config'][1]
 
         if len(remote_param):
             with self.impl.mysql_client.session() as session:
