cfd9166c66e68f3a7e2565a09c93ac7220797731
baishanglin <baishanglin@renrenche.com>
2020-01-02 14:24:20 +0800

    v0.0.3 BETA 版本 更新内置模块

--- bspider/master/controller/validators/project_form.py
+++ bspider/master/controller/validators/project_form.py
@@ -55,7 +55,7 @@ class AddForm(BaseForm):
 
 class UpdateForm(BaseForm):
     status = IntegerField()
-    description = StringField()
+    config = StringField()
     editor = StringField()
 
     def validate_config(self, value):
