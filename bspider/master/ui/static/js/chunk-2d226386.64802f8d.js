9eb5131ef0339e82058d2aaff8f1ae01c2dbafe3
baishanglin <baishanglin@renrenche.com>
2020-01-02 18:31:45 +0800

    v0.0.3 BETA 版本 UI 更新

---
+++ bspider/master/ui/static/js/chunk-2d226386.64802f8d.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d226386"],{e874:function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("code-detail",{attrs:{"is-edit":!0}})},c=[],i=n("bcdf"),l={name:"Edit",components:{CodeDetail:i["a"]}},o=l,s=n("2877"),d=Object(s["a"])(o,a,c,!1,null,null,null);t["default"]=d.exports}}]);
