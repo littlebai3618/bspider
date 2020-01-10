0c4fd5f5e3ea769154ace150694e410b52e5b4d3
baishanglin <baishanglin@renrenche.com>
2020-01-10 11:47:48 +0800

    v0.0.3 BETA 版本 修复 工作进程无法监控code 变化的bug

--- bspider/core/base_monitor.py
+++ bspider/core/base_monitor.py
@@ -70,7 +70,6 @@ class BaseMonitor(object):
 
             if worker_obj is None or worker_obj.sign != sign:
                 tmp_projects[project.project_id] = self.get_work_obj(project, sign=sign)
-                self.log.debug(f'success init project->project_id:{project.project_id}')
             else:
                 tmp_projects[info['id']] = worker_obj
 
