1e3d7df2e63abb789d4fa97b2c5bec7607b76bb7
baishanglin <baishanglin@renrenche.com>
2020-03-21 04:03:03 +0800

    v0.0.5 REALIZE 版本 修复代理池bug 增加request params 属性 修复ssl bug

--- bspider/core/base_monitor.py
+++ bspider/core/base_monitor.py
@@ -56,7 +56,7 @@ class BaseMonitor(object):
                 total_sum += info['rate']
 
             worker_obj = self.projects.get(info['id'])
-            self.log.info(info['config'], type(info['config']))
+            self.log.info(type(info['config']))
             project = Project(
                 settings=info['config'],
                 pipeline_serializer_method=self.code_id_to_content,
