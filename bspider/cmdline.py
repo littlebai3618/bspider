5d6420d5e9fba553ff48d12ac8a072d48dae8e20
baishanglin <baishanglin@renrenche.com>
2020-04-15 17:05:43 +0800

    v0.0.5 REALIZE 版本 优化命令行代码

--- bspider/cmdline.py
+++ bspider/cmdline.py
@@ -5,111 +5,101 @@ https://github.com/scrapy/scrapy/tree/master/scrapy/commands
 """
 import inspect
 import optparse
-import os
 import sys
+import os
 
 from bspider.commands import BSpiderCommand
 from bspider.config import FrameSettings
+from bspider.utils.conf import init_platform_env, PLATFORM_NAME_ENV, BSPIDER_VERSION_ENV
 from bspider.utils.exceptions import UsageError
 from bspider.utils.importer import walk_modules
-from bspider.utils.conf import init_platform_env, PLATFORM_NAME_ENV, BSPIDER_VERSION_ENV
 
 
-def _get_commands_from_module(module='bspider.commands'):
-    cmds = dict()
-    for mod in walk_modules(module):
-        for obj in vars(mod).values():
-            if inspect.isclass(obj) and \
-                    issubclass(obj, BSpiderCommand) and \
-                    obj.__module__ == mod.__name__ and \
-                    not obj == BSpiderCommand:
-                cmds[obj.__module__.split('.')[-1]] = obj()
-    return cmds
+class CommandLine(object):
+
+    def __init__(self):
+        self.cmds = self.__get_commands_from_module()
+        print(self.cmds)
+        self.args = list()
+
+    @staticmethod
+    def __get_commands_from_module(module='bspider.commands'):
+        cmds = dict()
+        for mod in walk_modules(module):
+            for obj in vars(mod).values():
+                if inspect.isclass(obj) and \
+                        issubclass(obj, BSpiderCommand) and \
+                        obj.__module__ == mod.__name__ and \
+                        not obj == BSpiderCommand:
+                    cmds[obj.__module__.split('.')[-1]] = obj()
+        return cmds
+
+    @staticmethod
+    def __print_header():
+        if os.environ.get(PLATFORM_NAME_ENV):
+            print(f"BSpider {os.environ[BSPIDER_VERSION_ENV]} - platform {os.environ[PLATFORM_NAME_ENV]}")
+        else:
+            print(f"BSpider {os.environ[BSPIDER_VERSION_ENV]} - no active platform")
+
+    @property
+    def command_name(self):
+        i = 0
+        for arg in self.args[1:]:
+            if not arg.startswith('-'):
+                del self.args[i]
+                return arg
+            i += 1
+
+    @init_platform_env
+    def execute(self, argv=None):
+        self.args = sys.argv if argv is None else argv
+        command_name = self.command_name
+        if not command_name:
+            self.__print_header()
+            print("Usage: bspider <command> [options] [args]")
+            print("Available commands:")
+            for cmdname, cmdclass in sorted(self.cmds.items()):
+                print("  %-13s %s" % (cmdname, cmdclass.short_desc()))
+            print('Use "bspider <command> -h" to see more info about a command')
+            sys.exit(0)
+
+        if not command_name in self.cmds:
+            self.__print_header()
+            print("Unknown command: %s\n" % command_name)
+            sys.exit(2)
+
+        if command_name == 'mkplatform' or command_name == 'version':
+            frame_settings = dict()
+        elif os.environ.get(PLATFORM_NAME_ENV):
+            frame_settings = FrameSettings()
+        else:
+            self.__print_header()
+            print('Please use bspider mkplatform <platform_name> to create a platform')
+            sys.exit(2)
+
+        command = self.cmds[command_name]
+        parser = optparse.OptionParser(formatter=optparse.IndentedHelpFormatter(), conflict_handler='resolve')
+        parser.prog = command_name
+        parser.usage = f"bspider {command_name} {command.syntax()}"
+        parser.description = command.long_desc()
+        command.add_options(parser)
+        command.frame_settings = frame_settings
+        try:
+            command.run(*parser.parse_args(self.args))
+        except UsageError:
+            parser.print_help()
+            sys.exit(2)
+        sys.exit(command.exitcode)
 
 
-def _pop_command_name(argv):
-    i = 0
-    for arg in argv[1:]:
-        if not arg.startswith('-'):
-            del argv[i]
-            return arg
-        i += 1
-
-
-def _print_header():
-    if os.environ.get(PLATFORM_NAME_ENV):
-        print(f"BSpider {os.environ[BSPIDER_VERSION_ENV]} - platform {os.environ[PLATFORM_NAME_ENV]}")
-    else:
-        print(f"BSpider {os.environ[BSPIDER_VERSION_ENV]} - no active platform")
-
-def _print_commands(cmds):
-    _print_header()
-    print("Usage: bspider <command> [options] [args]")
-    print("Available commands:")
-    for cmdname, cmdclass in sorted(cmds.items()):
-        print("  %-13s %s" % (cmdname, cmdclass.short_desc()))
-    print('Use "bspider <command> -h" to see more info about a command')
-
-
-def _print_unknown_command(cmdname):
-    _print_header()
-    print("Unknown command: %s\n" % cmdname)
-    print('Use "bspider" to see available commands')
-
-
-def _print_unknow_platform():
-    print(f"BSpider {os.environ[BSPIDER_VERSION_ENV]} - no platform")
-    print('Please use bspider mkplatform <platform_name> to create a platform')
-
-
-def run_cmd(cmd, parser, args, opts):
-    try:
-        cmd.run(args, opts)
-    except UsageError:
-        parser.print_help()
-        sys.exit(2)
-
-
-@init_platform_env
-def execute(argv=None):
+def execute(argv):
     """
     执行终端命令
     :param cmdname:
     :param args:
     :return:
     """
-    if argv is None:
-        argv = sys.argv
-    # 取出命令
-    cmdname = _pop_command_name(argv)
-    # 获取终端命令列表
-    cmds = _get_commands_from_module()
-    # 获取命令解析对象
-    parser = optparse.OptionParser(formatter=optparse.IndentedHelpFormatter(), conflict_handler='resolve')
-    if not cmdname:
-        _print_commands(cmds)
-        sys.exit(0)
-    if cmdname not in cmds:
-        _print_unknown_command(cmdname)
-        sys.exit(2)
-
-    if cmdname == 'mkplatform' or cmdname == 'version':
-        frame_settings = dict()
-    elif os.environ.get(PLATFORM_NAME_ENV):
-        frame_settings = FrameSettings()
-    else:
-        _print_unknow_platform()
-        sys.exit(2)
-
-    cmd = cmds[cmdname]
-    parser.prog = cmdname
-    parser.usage = f"bspider {cmdname} {cmd.syntax()}"
-    parser.description = cmd.long_desc()
-    cmd.add_options(parser)
-    opts, args = parser.parse_args(args=argv[1:])
-    cmd.frame_settings = frame_settings
-    run_cmd(cmd, parser, args, opts)
-    sys.exit(cmd.exitcode)
+    CommandLine().execute(argv)
 
 
 if __name__ == '__main__':
--- bspider/http/__init__.py
+++ bspider/http/__init__.py
@@ -1,10 +1,12 @@
 from .request import Request
 from .response import Response
 
+ERROR_REQUEST = Request('bspider://error')
+
 ERROR_RESPONSE = Response(
         url='bspider://error',
         status=599,
-        request=Request('bspider://error')
+        request=ERROR_REQUEST
     )
 
 __all__ = ['Request', 'Response', 'ERROR_RESPONSE']
