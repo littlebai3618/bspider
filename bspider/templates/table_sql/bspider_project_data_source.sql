0998b0bfc0796706a9ec7bb299addd6caf404a82
baishanglin <baishanglin@renrenche.com>
2020-05-29 17:51:19 +0800

    v0.0.8 修复模板语句bug

--- bspider/templates/table_sql/bspider_project_data_source.sql
+++ bspider/templates/table_sql/bspider_project_data_source.sql
@@ -8,7 +8,7 @@ CREATE TABLE `bspider_project_data_source` (
   KEY `idx_data_source_name` (`data_source_name`),
   KEY `idx_project_id` (`project_id`),
   CONSTRAINT `bspider_project_data_source_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `bspider_project` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `bspider_project_data_source_ibfk_2` FOREIGN KEY (`data_source_name`) REFERENCES `bspider_data_source` (`name`) ON DELETE CASCADE,
+  CONSTRAINT `bspider_project_data_source_ibfk_2` FOREIGN KEY (`data_source_name`) REFERENCES `bspider_data_source` (`name`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
