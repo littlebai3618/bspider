724202780985849cefe8e392df1d358a1db917f8
baishanglin <baishanglin@renrenche.com>
2020-07-13 14:14:39 +0800

    v0.0.8 调整 auth 模块日志输出路径

--- bspider/master/__init__.py
+++ bspider/master/__init__.py
@@ -1,6 +1,3 @@
 from bspider.utils.logger import LoggerPool
 
 log = LoggerPool().get_logger(key='master', fn='master', module='master')
-# 注册一个Auth模块的句柄
-LoggerPool().get_logger(key='api_auth', fn='master', module='api_auth')
-
