653afe0f63bc034818eb486515195da54f52c68b
baishanglin <baishanglin@renrenche.com>
2021-05-12 16:43:16 +0800

    修复配置文件解析异常

--- bspider/core/project.py
+++ bspider/core/project.py
@@ -231,8 +231,7 @@ class BaseModuleSettings(object):
     def __repr__(self):
         result = list()
         for module in self.__data:
-            for cls, _ in module:
-                result.append(str(cls))
+            result.append(str(module[0]))
         return '<{}: {} items>'.format(self.__class__.__name__, '->'.join(result))
 
     __str__ = __repr__
