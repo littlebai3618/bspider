596e29b61df76edd90b889dbc7d856dc810a03e8
baishanglin <baishanglin@renrenche.com>
2021-05-13 10:12:13 +0800

    c

--- bspider/core/my_tuple.py
+++ bspider/core/my_tuple.py
@@ -10,3 +10,14 @@ class TupleC(object):
     def __init__(self, cls: Any, params: dict):
         self.cls = cls
         self.params = params
+
+    def __iter__(self):
+        self.is_iter = True
+        return self
+
+    def __next__(self):
+        if self.is_iter:
+            self.is_iter = False
+            return self
+
+        raise StopIteration()
